8baa8384f227af0fe1d26ddb535c958c38502d59
Антон Цыганок <tantoros@yandex.ru>
2023-07-05 00:25:32 +0300

    add algorithm

---
+++ Yandex_Contest/data_structures/a_monitoring.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = int(input())
+# numbers = []  # Получил TL, переделал
+# for i in range(n):
+#     numbers.append(list(map(int, input().strip().split())))
+numbers = ''
+numbers = [input().split(' ') for j in range(n)]
+
+for i in range(m):
+    for j in range(n):
+        print(numbers[j][i], end=' ')
+    print()
