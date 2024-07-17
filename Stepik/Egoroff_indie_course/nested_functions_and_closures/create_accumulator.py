1c74110cac63ae8dfe203b5041f05da2978752d6
Антон Цыганок <tantoros@yandex.ru>
2024-07-17 17:00:48 +0300

    add create_accumulator

---
+++ Stepik/Egoroff_indie_course/nested_functions_and_closures/create_accumulator.py
@@ -0,0 +1,36 @@
+"""
+Ваша задача создать функцию-замыкание create_accumulator,
+она должна накапливать(суммировать) в себе все значения,
+которые ей будут переданы, при создании сумма должна быть равна нулю.
+Посмотрите пример ниже:
+
+summator_1 = create_accumulator()
+print(summator_1(1)) # печатает 1
+print(summator_1(5)) # печатает 6
+print(summator_1(2)) # печатает 8
+
+summator_2 = create_accumulator()
+print(summator_2(3)) # печатает 3
+print(summator_2(4)) # печатает 7
+При каждом вызове должна возвращаться накопленная сумма,
+которая хранится в замыкании.
+
+Обратите внимание, что объекты из примера
+summator_1 и summator_2 хранят и накапливают свои собственные суммы.
+
+Необходимо только определить функцию-замыкание create_accumulator,
+остальное мы сделаем за вас
+"""
+
+from typing import Callable
+
+
+def create_accumulator() -> Callable[[int], int]:
+    counter = 0
+
+    def inner(value: int) -> int:
+        nonlocal counter
+        counter += value
+        return counter
+
+    return inner
