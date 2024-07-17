0442c9c0153b387b43402df96bc5f4e8c8dc261b
Антон Цыганок <tantoros@yandex.ru>
2024-07-17 16:52:32 +0300

    add outer

---
+++ Stepik/Egoroff_indie_course/nested_functions_and_closures/outer.py
@@ -0,0 +1,39 @@
+"""
+Исправьте код с предыдущего задания так, чтобы на экран вывело bye и hello
+Код с предыдущего задания:
+
+------------------------------
+
+
+def outer() -> None:
+    def say_hello() -> None:
+        print('hello')
+
+    def say_bye() -> None:
+        print('bye')
+
+
+say_hello()
+say_bye()
+
+------------------------------
+
+Sample Input:
+Sample Output:
+bye
+hello
+"""
+
+
+def outer() -> None:
+    def say_hello() -> None:
+        print('hello')
+
+    def say_bye() -> None:
+        print('bye')
+
+    say_bye()
+    say_hello()
+
+
+outer()
