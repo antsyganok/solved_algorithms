76b271c4b1ab88713ad294c171303b9911278196
Антон Цыганок <tantoros@yandex.ru>
2024-12-03 02:01:25 +0300

    edit palindrome_1

--- Other/palindrome/palindrome_1.py
+++ Other/palindrome/palindrome_1.py
@@ -11,6 +11,6 @@ import re
 
 
 source = re.sub(r'[^\w\s]', '', input().lower().replace(' ', ''))
-mid = int(len(source) / 2)
+mid = len(source) // 2
 
 print('Палиндром!!!' if source[:mid] == source[:mid:-1] else 'Не палиндром!')
