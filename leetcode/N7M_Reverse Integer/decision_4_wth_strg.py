3271a23589f51cc7d037b96a3a2dc6a0390c354c
Антон Цыганок <tantoros@yandex.ru>
2025-01-22 16:15:15 +0300

    add lcode problem 7

---
+++ leetcode/N7M_Reverse Integer/decision_4_wth_strg.py
@@ -0,0 +1,19 @@
+""" Runtime:
+41 ms
+Beats 37.05%
+
+Memory:
+17.86 MB
+Beats 23.45%
+"""
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        if x < 0:
+            x = int(str(x)[:0:-1]) * -1
+            return x if -2147483648 <= x else 0
+        else:
+            x = int(str(x)[::-1])
+            return x if 2147483647 >= x else 0
---
+++ leetcode/N7M_Reverse Integer/decision_6_wth_strg.py
@@ -0,0 +1,8 @@
+""" Runtime:
+31 ms
+Beats 91.74%
+
+Memory:
+17.99 MB
+Beats 10.79%
+"""
