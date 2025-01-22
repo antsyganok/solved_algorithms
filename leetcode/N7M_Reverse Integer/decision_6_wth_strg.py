d75b98e100b8789a65f717decb387f0fef7d88c9
Антон Цыганок <tantoros@yandex.ru>
2025-01-22 16:18:15 +0300

    add lcode problem 7

--- leetcode/N7M_Reverse Integer/decision_6_wth_strg.py
+++ leetcode/N7M_Reverse Integer/decision_6_wth_strg.py
@@ -3,6 +3,16 @@
 Beats 91.74%
 
 Memory:
-17.99 MB
-Beats 10.79%
+17.76 MB
+Beats 36.43%
 """
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            x = int(str(x)[:0:-1]) * -1
+        else:
+            x = int(str(x)[::-1])
+
+        return x if x.bit_length() < 32 else 0
