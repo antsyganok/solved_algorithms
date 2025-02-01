be53ceb0b29fe8080d697b8e2048d35a24ee6450
Антон Цыганок <tantoros@yandex.ru>
2025-02-01 13:08:07 +0300

    add p41 fmp

---
+++ leetcode/N41H_First_missing_positive/decision_1.py
@@ -0,0 +1,20 @@
+"""
+23 ms
+Beats 87.61%
+
+Memory:
+34.74 MB
+Beats 11.69%
+"""
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        mx = min(max(nums, default=0), int(3e9))
+
+        for i in range(1, mx):
+            if i not in nums:
+                return i
+        return 1 if mx < 0 else mx + 1
