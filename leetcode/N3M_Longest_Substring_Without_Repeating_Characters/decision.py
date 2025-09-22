57c7785bd994d47ba760232a2e0291650efdbdf3
Антон Цыганок <tantoros@yandex.ru>
2025-09-22 17:21:21 +0300

    chge rme

--- leetcode/N3M_Longest_Substring_Without_Repeating_Characters/decision.py
+++ leetcode/N3M_Longest_Substring_Without_Repeating_Characters/decision.py
@@ -1,5 +1,13 @@
 """
+#### 3. Longest Substring Without Repeating Characters
+Medium
 
+Given a string s, find the length of the longest substring
+without duplicate characters.
+
+Constraints:
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
 """
 
 
