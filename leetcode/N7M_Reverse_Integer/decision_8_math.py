e49dab1dc202cc273fb5f112537dc2d4ce1efd23
Антон Цыганок <tantoros@yandex.ru>
2025-09-25 13:01:05 +0300

    chge rme

---
+++ leetcode/N7M_Reverse_Integer/decision_8_math.py
@@ -0,0 +1,26 @@
+"""
+Runtime:
+31 ms
+Beats 93.66%
+
+Memory:
+17.49 MB
+Beats 99.79%
+"""
+
+
+def reverse(self, x: int) -> int:
+    INT_MAX = 2**31 - 1
+    INT_MIN = -2**31
+
+    reslt = 0
+    sign = -1 if x < 0 else 1
+    x = abs(x)
+
+    while x > 0:
+        reslt = reslt * 10 + x % 10
+        x //= 10
+
+    reslt *= sign
+
+    return reslt if INT_MIN <= reslt <= INT_MAX else 0
