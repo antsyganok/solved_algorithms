a3fca62dee4dfa49fa507c05f9e14125f27ff61f
Антон Цыганок <tantoros@yandex.ru>
2025-09-30 16:21:42 +0300

    chge rme

--- leetcode/N7M_Reverse_Integer/decision_6_wth_strg.py
+++ leetcode/N7M_Reverse_Integer/decision_6_wth_strg.py
@@ -8,11 +8,10 @@ Beats 36.43%
 """
 
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x < 0:
-            x = int(str(x)[:0:-1]) * -1
-        else:
-            x = int(str(x)[::-1])
+def reverse(x: int) -> int:
+    if x < 0:
+        x = int(str(x)[:0:-1]) * -1
+    else:
+        x = int(str(x)[::-1])
 
-        return x if x.bit_length() < 32 else 0
+    return x if x.bit_length() < 32 else 0
