1ea5573075a47d1fdfc71c145f2058f6c4e3ef5b
Антон Цыганок <tantoros@yandex.ru>
2025-09-30 16:23:48 +0300

    chge rme

--- leetcode/N7M_Reverse_Integer/naive.py
+++ leetcode/N7M_Reverse_Integer/naive.py
@@ -9,11 +9,10 @@ Beats 36.43%
 """
 
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x < 0:
-            x = int(str(x)[:0:-1]) * -1
-            return x if (2 ** 31) * -1 <= x else 0
-        else:
-            x = int(str(x)[::-1])
-            return x if (2 ** 31) - 1 >= x else 0
+def reverse(self, x: int) -> int:
+    if x < 0:
+        x = int(str(x)[:0:-1]) * -1
+        return x if (2 ** 31) * -1 <= x else 0
+    else:
+        x = int(str(x)[::-1])
+        return x if (2 ** 31) - 1 >= x else 0
