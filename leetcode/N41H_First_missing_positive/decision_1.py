d4e5a72099245d1debc7777ec1f90c15713d74f1
Антон Цыганок <tantoros@yandex.ru>
2025-09-30 16:26:36 +0300

    chge rme

--- leetcode/N41H_First_missing_positive/decision_1.py
+++ leetcode/N41H_First_missing_positive/decision_1.py
@@ -9,12 +9,11 @@ Beats 11.69%
 from typing import List
 
 
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        mx = min(max(nums, default=0), int(3e9))
+def firstMissingPositive(nums: List[int]) -> int:
+    nums = set(nums)
+    mx = min(max(nums, default=0), int(3e9))
 
-        for i in range(1, mx):
-            if i not in nums:
-                return i
-        return 1 if mx < 0 else mx + 1
+    for i in range(1, mx):
+        if i not in nums:
+            return i
+    return 1 if mx < 0 else mx + 1
