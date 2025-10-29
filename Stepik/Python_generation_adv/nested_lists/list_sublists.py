4d30f31a701dc43a8ce719f2cde995f50f85698f
Антон Цыганок <tantoros@yandex.ru>
2025-10-29 15:15:11 +0300

    chge rdme

--- Stepik/Python_generation_adv/nested_lists/list_sublists.py
+++ Stepik/Python_generation_adv/nested_lists/list_sublists.py
@@ -30,10 +30,12 @@ Sample Input 1:
 a b
 Sample Output 1:
 [[], ['a'], ['b'], ['a', 'b']]
+
 Sample Input 2:
 a b v
 Sample Output 2:
 [[], ['a'], ['b'], ['v'], ['a', 'b'], ['b', 'v'], ['a', 'b', 'v']]
+
 Sample Input 3:
 a
 Sample Output 3:
