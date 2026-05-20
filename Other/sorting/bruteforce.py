5e598565631a921ea2948090d5f202187204c56a
Антон Цыганок <tantoros@yandex.ru>
2026-05-20 14:08:43 +0300

    add sorting

--- Other/sorting/bruteforce.py
+++ Other/sorting/bruteforce.py
@@ -3,6 +3,7 @@
 def bruteforce(data: list[int]) -> list[int]:
     """
     Сортирует массив целых чисел методом перебора (выбора).
+
     Алгоритм находит минимальные элементы, извлекает их из копии
     исходного списка и формирует новый, упорядоченный массив.
     Оригинальный список при этом не изменяется.
@@ -11,9 +12,7 @@ def bruteforce(data: list[int]) -> list[int]:
     new_numbers = []
 
     while len(numbers) > 0:
-
         minval = 0
-
         for i in range(1, len(numbers)):
             if numbers[i] < numbers[minval]:
                 minval = i
@@ -23,7 +22,6 @@ def bruteforce(data: list[int]) -> list[int]:
 
     return new_numbers
 
-
-arr_numbers = [42, 17, 3, 88, 5, 72, 10, 3, 56, 29]
-
-print(bruteforce(arr_numbers))
+if __name__ == "__main__":
+    arr_numbers = [42, 17, 3, 88, 5, 72, 10, 3, 56, 29]
+    print(bruteforce(arr_numbers))
