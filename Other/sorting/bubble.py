227edc5cbd5436c9bad88558cdd7adb94659e02f
Антон Цыганок <tantoros@yandex.ru>
2026-05-20 15:46:27 +0300

    add bubble sort

--- Other/sorting/bubble.py
+++ Other/sorting/bubble.py
@@ -1,5 +1,7 @@
+from typing import Union
 
-def bubble_sort(data: list[int]) -> list[int]:
+
+def bubble_sort(data: list[Union[int, float]]) -> list[Union[int, float]]:
     """
     Сортирует массив чисел методом пузырька (Bubble Sort) in-place.
 
@@ -11,13 +13,17 @@ def bubble_sort(data: list[int]) -> list[int]:
     numbers = data.copy()
 
     for iteration in range(len(numbers) - 1):
+        flag = False
         for i in range(1, len(numbers) - iteration):
             if numbers[i-1] > numbers[i]:
                 numbers[i-1], numbers[i] = numbers[i], numbers[i-1]
+                flag =True
+        if not flag:
+            break
 
     return numbers
 
 
 if __name__ == "__main__":
-    arr_numbers = [42, 17, 3, 88, -2, 1000, 0, 5, 72, 10, 1.5, 3, 56, 29]
+    arr_numbers = [42, 17, 3, 88, -2, 1002, 0, 5, 72, 10, 1.5, 3, 56, 29]
     print(bubble_sort(arr_numbers))
