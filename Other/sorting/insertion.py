42e6300fb70c9ae1895157fea7cd4ace16622fd1
Антон Цыганок <tantoros@yandex.ru>
2026-07-29 16:30:34 +0300

    add ins sort

---
+++ Other/sorting/insertion.py
@@ -0,0 +1,22 @@
+
+
+def InsertionSort(data: list[int]) -> list[int]:
+    """
+    сортировка вставками
+    """
+    numbers = data.copy()
+
+    for i in range(1, len(numbers)):
+        elem = numbers[i]
+        j = i-1
+
+        while j >= 0 and numbers[j] > elem:
+            numbers[j+1] = numbers[j]
+            j -= 1
+
+        numbers[j + 1] = elem
+    return numbers
+
+if __name__ == "__main__":
+    arr_numbers = [42, 17, 3, 88, -2, 1002, 0, 5, 72, 10, 1.5, 3, 56, 29]
+    print(InsertionSort(arr_numbers))
