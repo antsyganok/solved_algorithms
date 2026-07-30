6e0e47e6ee7a1435938f3b366b7ee1dce5fa7b9a
Антон Цыганок <tantoros@yandex.ru>
2026-07-30 14:48:14 +0300

    edt fn nme

--- Other/sorting/selection.py
+++ Other/sorting/selection.py
@@ -1,6 +1,6 @@
 
 
-def selectionSort(data: list[int]) -> list[int]:
+def selection_sort(data: list[int]) -> list[int]:
     """
     Сортирует массив целых чисел методом перебора (выбора).
 
@@ -23,4 +23,4 @@ def selectionSort(data: list[int]) -> list[int]:
 
 if __name__ == "__main__":
     arr_numbers = [42, 17, 3, 88, -2, 1002, 0, 5, 72, 10, 1.5, 3, 56, 29]
-    print(selectionSort(arr_numbers))
+    print(selection_sort(arr_numbers))
